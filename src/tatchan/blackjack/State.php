f4cf6ec12ef8e94af09ccfd450147ebe48cee641
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-05-04 08:26:41 +0900

    sleep

--- src/tatchan/blackjack/State.php
+++ src/tatchan/blackjack/State.php
@@ -26,6 +26,25 @@ class State
         return $count;
     }
 
+    //自分のスコアを計算するコード
+    public function getScore(): int {
+        $score = 0;
+        $aceCount = 0;
+        foreach ($this->cards->getAll() as $card) {
+            $num = $card->getNumber();
+            if ($num >= 10) $num = 10;
+            if ($num == 1) $aceCount++;
+            else $score += $num;
+        }
+        if ($aceCount == 0) return $score;
+
+        $aceScore = 11 + $aceCount - 1;
+        if ($aceScore + $score > 21) {
+            $aceScore = $aceCount;
+        }
+        return $score + $aceScore;
+    }
+
     public function isBust(): bool {
         return $this->getCount() > 21;
     }
