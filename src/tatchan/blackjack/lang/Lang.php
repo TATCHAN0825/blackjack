e3b04fff8e316eb5a3916ad0c673c465a5832c41
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-05-06 19:53:18 +0900

    langわすれてた

---
+++ src/tatchan/blackjack/lang/Lang.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace tatchan\blackjack\lang;
+
+use pocketmine\lang\BaseLang;
+use pocketmine\lang\TextContainer;
+
+class Lang
+{
+    /** @var BaseLang */
+    private static $lang;
+
+    public static function init(BaseLang $lang): void {
+        self::$lang = $lang;
+    }
+
+    public static function get(): BaseLang {
+        return self::$lang;
+    }
+
+    /**
+     * 翻訳する
+     * @param $text TextContainer|string
+     */
+    public static function t($text, array $params = []): string {
+        if ($text instanceof TextContainer) {
+            return self::$lang->translate($text);
+        } else {
+            return self::$lang->translateString($text, $params);
+        }
+    }
+}
