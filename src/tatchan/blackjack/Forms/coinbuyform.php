890529147de4935d40f8b339eee3da00dbd3dc6b
tatchan <tatsu.jou.0825.line2@gmail.com>
2021-05-08 23:29:27 +0900

    coinsellとおんなじような処理するようにした

--- src/tatchan/blackjack/Forms/coinbuyform.php
+++ src/tatchan/blackjack/Forms/coinbuyform.php
@@ -8,6 +8,7 @@ use dktapps\pmforms\CustomFormResponse;
 use dktapps\pmforms\element\Input;
 use PJZ9n\MoneyConnector\MoneyConnectorUtils;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 use tatchan\blackjack\CoinManager;
 use tatchan\blackjack\lang\Lang;
 use tatchan\blackjack\Main;
@@ -18,26 +19,40 @@ class coinbuyform extends AbstractCustomForm
     /** @var int */
     private $coin, $coinrate;
 
-    public function __construct(Player $player, string $err = null) {
+    public function __construct(Player $player, array $messages = []) {
         $money = MoneyConnectorUtils::getConnectorByDetect()->myMoney($player);
         $this->coin = CoinManager::getInstance()->get($player->getName());
         $this->coinrate = Main::getInstance()->getConfig()->get("coinrate");
-        parent::__construct(Lang::t("coin.buy.form"), [new Input(Lang::t("coin.buy"), Lang::t("number.coins.buy") . "\n" . Lang::t("coin.mycoin", [$this->coin]) . "\n" . Lang::t("economy.mymoney", [$money]) . "\n" . $err)]);
-
+        parent::__construct(
+            Lang::t("coin.buy.form"),
+            [
+                new Input("coinbuy", Lang::t("number.coins.buy") . "\n"
+                    . Lang::t("coin.mycoin", [$this->coin]) . "\n"
+                    . Lang::t("economy.mymoney", [$money]) . "\n"
+                    . implode(TextFormat::EOL, $messages)
+                )
+            ]
+        );
     }
 
     public function onSubmit(Player $player, CustomFormResponse $response): void {
-        if (Main::getInstance()->is_natural($response->getString(Lang::t("coin.buy")))) {
-            $paymoney = $this->coinrate * (int)$response->getString(Lang::t("coin.buy"));
-            if (MoneyConnectorUtils::getConnectorByDetect()->myMoney($player) >= $paymoney) {
-                MoneyConnectorUtils::getConnectorByDetect()->reduceMoney($player, $paymoney);
-                CoinManager::getInstance()->addcoin($player->getName(), (int)$response->getString(Lang::t("coin.buy")));
-            } else {
-                $player->sendMessage("§e" . Lang::t("not.enough.money"));
-            }
-        } else {
-            $err = "§c" . Lang::t("enter.natural.number");
-            $player->sendForm(new coinbuyform($player, $err));
+        $amount = $response->getString("coinbuy");
+        $paymoney = $this->coinrate * (int)$response->getString("coinbuy");
+        if (!is_numeric($amount)) {
+            $player->sendForm(new coinbuyform($player, [Lang::t("enter.natural.number")]));
+            return;
         }
+        $amount = (int)$amount;
+        if ($amount <= 0) {
+            $player->sendForm(new coinbuyform($player, [Lang::t("make.it.1")]));
+        }
+        if (!MoneyConnectorUtils::getConnectorByDetect()->myMoney($player) >= $paymoney) {
+            $player->sendForm(new coinbuyform($player, [Lang::t("not.enough.money")]));
+            return;
+        }
+        MoneyConnectorUtils::getConnectorByDetect()->reduceMoney($player, $paymoney);
+        MoneyConnectorUtils::getConnectorByDetect()->addMoney($player, $amount);
+        $player->sendMessage(Lang::t("coin.buy.it"));
     }
+
 }
